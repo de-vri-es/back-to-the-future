18724f17a7eb9f40fd0337b8768049ede3845ac5
Maarten de Vries <maarten@de-vri.es>
2018-12-23 23:12:08 +0100

    Add crate documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,35 @@
-#![feature(arbitrary_self_types)]
+//! # `std` and `futures` interoperability
+//! This crate implements adapters for the two different future types: [`std::future::Future`] and [`futures::Future`].
+//! You can should be able to seamlessly convert the one into the other.
+//! The aim if to be able to use new async/await syntax with existing [`futures::Future`] infrastructure.
+//!
+//! Keep in mind that much of the used features are still unstable and only available on nightly with feature gates.
+//!
+//! A simple example:
+//! ```
+//! #![feature(async_await)]
+//! #![feature(await_macro)]
+//! #![feature(futures_api)]
+//!
+//! use std::time::{Duration, Instant};
+//! use tokio::timer::Delay;
+//!
+//! use back_to_the_future::futures_await;
+//! use back_to_the_future::BoxIntoFutures;
+//! use back_to_the_future::IntoFutures;
+//!
+//! fn main() {
+//! 	let f = async {
+//! 		futures_await!(Delay::new(Instant::now() + Duration::new(0, 10))).unwrap();
+//! 		Ok(())
+//! 	};
+//! 	tokio::run(f.box_into_futures());
+//! }
+//! ```
 
+#![doc(html_root_url = "https://docs.rs/back_to_the_future/0.1.0")]
+
+#![feature(arbitrary_self_types)]
 #![feature(async_await)]
 #![feature(await_macro)]
 #![feature(futures_api)]
